2342ec731e3d31a8f1dd6eb6e0015d64dc82036d
hoangkha442 <125436413+hoangkha442@users.noreply.github.com>
2023-12-30 15:37:40 +0700

    Database

---
+++ printerest.sql
@@ -0,0 +1,102 @@
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+CREATE TABLE `binh_luan` (
+  `binh_luan_id` int NOT NULL AUTO_INCREMENT,
+  `nguoi_dung_id` int DEFAULT NULL,
+  `hinh_id` int DEFAULT NULL,
+  `ngay_binh_luan` date DEFAULT NULL,
+  `noi_dung` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`binh_luan_id`),
+  KEY `nguoi_dung_id` (`nguoi_dung_id`),
+  KEY `hinh_id` (`hinh_id`),
+  CONSTRAINT `binh_luan_ibfk_1` FOREIGN KEY (`nguoi_dung_id`) REFERENCES `nguoi_dung` (`nguoi_dung_id`),
+  CONSTRAINT `binh_luan_ibfk_2` FOREIGN KEY (`hinh_id`) REFERENCES `hinh_anh` (`hinh_id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `hinh_anh` (
+  `hinh_id` int NOT NULL AUTO_INCREMENT,
+  `ten_hinh` varchar(255) DEFAULT NULL,
+  `duong_dan` varchar(255) DEFAULT NULL,
+  `mo_ta` varchar(255) DEFAULT NULL,
+  `nguoi_dung_id` int DEFAULT NULL,
+  PRIMARY KEY (`hinh_id`),
+  KEY `nguoi_dung_id` (`nguoi_dung_id`),
+  CONSTRAINT `hinh_anh_ibfk_1` FOREIGN KEY (`nguoi_dung_id`) REFERENCES `nguoi_dung` (`nguoi_dung_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `luu_anh` (
+  `nguoi_dung_id` int NOT NULL,
+  `hinh_id` int NOT NULL,
+  `ngay_luu` date DEFAULT NULL,
+  PRIMARY KEY (`nguoi_dung_id`,`hinh_id`),
+  KEY `luu_anh_ibfk_2` (`hinh_id`),
+  CONSTRAINT `luu_anh_ibfk_1` FOREIGN KEY (`nguoi_dung_id`) REFERENCES `nguoi_dung` (`nguoi_dung_id`),
+  CONSTRAINT `luu_anh_ibfk_2` FOREIGN KEY (`hinh_id`) REFERENCES `hinh_anh` (`hinh_id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `nguoi_dung` (
+  `nguoi_dung_id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) DEFAULT NULL,
+  `mat_khau` varchar(255) DEFAULT NULL,
+  `ho_ten` varchar(255) DEFAULT NULL,
+  `tuoi` int DEFAULT NULL,
+  `anh_dai_dien` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`nguoi_dung_id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO `binh_luan` (`binh_luan_id`, `nguoi_dung_id`, `hinh_id`, `ngay_binh_luan`, `noi_dung`) VALUES
+(1, 1, 1, '2023-01-01', 'This is a comment on the first photo.');
+INSERT INTO `binh_luan` (`binh_luan_id`, `nguoi_dung_id`, `hinh_id`, `ngay_binh_luan`, `noi_dung`) VALUES
+(4, 4, 1, '2023-12-14', 'test function post comment');
+INSERT INTO `binh_luan` (`binh_luan_id`, `nguoi_dung_id`, `hinh_id`, `ngay_binh_luan`, `noi_dung`) VALUES
+(8, 4, 4, '2023-12-16', 'test function comment FE');
+INSERT INTO `binh_luan` (`binh_luan_id`, `nguoi_dung_id`, `hinh_id`, `ngay_binh_luan`, `noi_dung`) VALUES
+(12, 4, 4, '2023-12-16', 'ảnh đẹp quá'),
+(13, 4, 4, '2023-12-16', '123'),
+(14, 4, 4, '2023-12-16', '123'),
+(15, 4, 5, '2023-12-17', 'ảnh đẹp đó');
+
+INSERT INTO `hinh_anh` (`hinh_id`, `ten_hinh`, `duong_dan`, `mo_ta`, `nguoi_dung_id`) VALUES
+(1, 'Beach Photo', 'pexels-vlad-alexandru-popa-1402787.jpg', 'A photo of a beach', 1);
+INSERT INTO `hinh_anh` (`hinh_id`, `ten_hinh`, `duong_dan`, `mo_ta`, `nguoi_dung_id`) VALUES
+(4, 'test function upload', 'pexels-vlad-alexandru-popa-1402787.jpg', 'this\'s a picture', 4);
+INSERT INTO `hinh_anh` (`hinh_id`, `ten_hinh`, `duong_dan`, `mo_ta`, `nguoi_dung_id`) VALUES
+(5, 'test function upload', '1702711478475_pexels-vlad-alexandru-popa-1402787.jpg', 'this\'s a picture', 4);
+INSERT INTO `hinh_anh` (`hinh_id`, `ten_hinh`, `duong_dan`, `mo_ta`, `nguoi_dung_id`) VALUES
+(6, 'test function upload', '1702711544752_pexels-vlad-alexandru-popa-1402787.jpg', 'this\'s a picture', 4),
+(25, 'test function upload', '1702713570001_1702713440282_HK221.jpg', 'this\'s a picture', 4),
+(26, 'test function upload', '1702713580890_Thiết Bị Y Tế.png', 'this\'s a picture', 4),
+(27, 'test function upload', '1702713761636_Thiết Bị Y Tế.png', 'this\'s a picture', 4),
+(28, 'test function upload', '1702714454721_Thiết Bị Y Tế.png', 'this\'s a picture', 4),
+(30, 'abc', '1702801047277_HK221.jpg', 'abc', 4),
+(31, 'oke', '1702801159057_chuKy-removebg-preview.png', 'oke', 4);
+
+INSERT INTO `luu_anh` (`nguoi_dung_id`, `hinh_id`, `ngay_luu`) VALUES
+(2, 1, '2023-01-04');
+INSERT INTO `luu_anh` (`nguoi_dung_id`, `hinh_id`, `ngay_luu`) VALUES
+(4, 1, '2023-01-04');
+
+
+INSERT INTO `nguoi_dung` (`nguoi_dung_id`, `email`, `mat_khau`, `ho_ten`, `tuoi`, `anh_dai_dien`) VALUES
+(1, 'user1@example.com', 'hashed_password_1', 'Nguyen Van A', 30, 'avatar1.jpg');
+INSERT INTO `nguoi_dung` (`nguoi_dung_id`, `email`, `mat_khau`, `ho_ten`, `tuoi`, `anh_dai_dien`) VALUES
+(2, 'user2@example.com', 'hashed_password_2', 'Tran Thi B', 25, 'avatar2.jpg');
+INSERT INTO `nguoi_dung` (`nguoi_dung_id`, `email`, `mat_khau`, `ho_ten`, `tuoi`, `anh_dai_dien`) VALUES
+(4, 'kha@gmail.com', '$2b$10$zIRd7dUhZUXkoXWn4ULN8OBK3i4iR93xR.PLn2J1fOZIoL9udtLiG', 'ChauHoangKha', 22, '1702801047277_HK221.jpg');
+
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
